ea6d10a7feed89843c4b9d325a930bf0e8669335
Gusttavomarinho <gusttavomarinho@gmail.com>
2014-10-09 00:12:51 -0300

    Criando Hellow word no python
    Algoritmo do curso python para zumbis , indicado pelo pessoal da
    Evolux.net.br
    Revisando e melhorando os conceitos em python

---
+++ hellow-word.py
@@ -0,0 +1,7 @@
+#Criando Hellow word no python 09-10-2014
+print ('Hellow Word')
+
+#      :::INFO::
+
+#Algoritmo do curso python para zumbis , indicado pelo pessoal da Evolux.net.br
+#Revisando e melhorando os conceitos em python

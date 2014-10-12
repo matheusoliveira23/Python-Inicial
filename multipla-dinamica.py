6d9269d25a36f52c8d38f0a9a8ddd535c7b6d54c
Gusttavomarinho <gusttavomarinho@gmail.com>
2014-10-12 16:10:34 -0300

    add new codigo multipla

---
+++ multipla-dinamica.py
@@ -0,0 +1,23 @@
+#Brincando com variaveis dinamicas e atribuição multipla
+
+a = 1
+b = 2
+#imprimindo os valores normal
+print('O valor de "A" é : %i , python e show'%a)
+print('O valor de "B" é : %i , python e show'%b)
+#Trocando os valores
+a,b = b,a
+print('========================================')
+#imprimindo valores trocados
+print('O valor de "A" é : %i , python e show'%a)
+print('O valor de "B" é : %i , python e show'%b)
+#imprimindo uma palavra com uma letra em cada variavel
+a,b,c,d,e,f,g, ='Gustavo'
+print('========================================')
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)

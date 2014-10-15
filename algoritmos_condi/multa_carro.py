c1cc4583ceea3009a28f2cef8a05dc44aac1918b
matheusoliveira23 <matheuscavalcante5b@gmail.com>
2014-10-15 19:44:16 -0300

    Update multa_carro.py

--- algoritmos_condi/multa_carro.py
+++ algoritmos_condi/multa_carro.py
@@ -1,7 +1,7 @@
 #Algoritmo para multar os carros kkkkk 'So uma brincadeira
 
 #Pegando os valores
-velocidade = int(input('Digie a velocidade do carro "Valor inteiro":'))
+velocidade = int(input('Digie a velocidade do carro "Valor inteiro":'))  #vms tentar pegar um float?
 if velocidade > 110:
     print('Voce foi multado :(')
     multa = (velocidade - 110)*5
